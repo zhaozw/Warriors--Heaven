b6c3e7442465d0a25d2912b4104da38480f0920c
jackieju <jackie.ju@gmail.com>
2012-04-12 10:46:25 +0800

    c

--- server/wh/db/migrate/20120325041039_create_usereqs.rb
+++ server/wh/db/migrate/20120325041039_create_usereqs.rb
@@ -17,20 +17,20 @@ class CreateUsereqs < ActiveRecord::Migration
   end
 end
 
-=begin                                             
-  insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 1, 'sword', -1, "hand left", NULL, NULL);
-  insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 2, 'blade', -1, "hand right", NULL, NULL);
+ActiveRecord::Base.connection.execute("                                            
+  insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 1, 'sword', -1, 'hand left', NULL, NULL);
+  insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 2, 'blade', -1, 'hand right', NULL, NULL);
   insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 3, 'armo', 0, NULL, NULL, NULL);
   insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 4, 'boots', 1, NULL, NULL, NULL);
   insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 5, 'ring', 2, NULL, NULL, NULL);
   insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 6, 'necklace', 3, NULL, NULL, NULL);
   insert into usereqs values(null, 1, 'd434740f4ff4a5e758d4f340d7a5f467', 7, 'cap', 4, NULL, NULL, NULL);
   
-  insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 1, 'sword', -1, "hand left", NULL, NULL);
-  insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 2, 'blade', -1, "hand right", NULL, NULL);
+  insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 1, 'sword', -1, 'hand left', NULL, NULL);
+  insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 2, 'blade', -1, 'hand right', NULL, NULL);
   insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 3, 'armo', 0, NULL, NULL, NULL);
   insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 4, 'boots', 1, NULL, NULL, NULL);
   insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 5, 'ring', 2, NULL, NULL, NULL);
   insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 6, 'necklace', 3, NULL, NULL, NULL);
   insert into usereqs values(null, 9, '1320346951bf2bc6293fb70cc2a71a05', 7, 'cap', 4, NULL, NULL, NULL);
-=end                                   
+")                                   
