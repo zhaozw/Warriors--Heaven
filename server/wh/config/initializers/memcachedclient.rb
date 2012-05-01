393183f30bec5f12e7065c73ae31f4a03d917a79
jackieju <jackie.ju@gmail.com>
2012-05-02 07:52:40 +0800

    c

---
+++ server/wh/config/initializers/memcachedclient.rb
@@ -0,0 +1,8 @@
+require 'memcache.rb'
+    mcd_default_options = {
+            :namespace => 'game:user',
+            :memcache_server => 'localhost:11211'
+    }
+
+       
+    $memcached= MemCache.new(mcd_default_options[:memcache_server], mcd_default_options)
