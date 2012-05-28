e4e0047acaf4f08d1b830e287f4546cc48061164
jackieju <jackie.ju@gmail.com>
2012-05-28 22:36:56 +0800

    c

---
+++ server/wh/cal_max_skill_level.rb
@@ -0,0 +1,9 @@
+level =  43
+a = 0
+for i in 0..level
+	a += i*i*i
+end
+exp = a
+fullskill=((exp*10)**(1/3.0)).to_i
+p "level=#{level}, exp=#{exp}, fullskill=#{fullskill}"
+p "exp for skill level #{fullskill} is #{fullskill**3/10}"
