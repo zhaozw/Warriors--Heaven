d5bdc7735618ad692f619e861873ced3f8d4e361
jackieju <jackie.ju@gmail.com>
2012-11-03 05:00:11 +0800

    config log level and rotation

--- server/wh/config/environments/production.rb
+++ server/wh/config/environments/production.rb
@@ -26,3 +26,15 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+#每天切分一次日志文件
+
+config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}#{Date.today.to_s}.log", "daily")
+#按日志文件大小切分,每50M切分一次（即每50M时轮换一次）
+#config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}1.log", 2, 51200000)
+
+
+#必须加这一句，不然会以debug模式打印日志
+config.logger.level = Logger::INFO 
+#！注意： config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}1.log", 2, 51200000)  中
+#{Rails.env}1.log  这句代码，即找到当前启动模式的日志文件，若写成#{Rails.env}.log时，会报错，提示无权限修改
